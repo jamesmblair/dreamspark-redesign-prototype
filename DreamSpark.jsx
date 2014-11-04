01015633f704425e2f23bf587106c39fceeb9078
James Blair <james.blair@outlook.com>
2014-11-03 21:36:13 -0500

    added details page

--- DreamSpark.jsx
+++ DreamSpark.jsx
@@ -23,22 +23,24 @@ var DreamSpark = React.createClass({
 		};
 	},
 	componentDidMount: function() {
-		var pages = {
-			'home' : <HomePage navigate={this.handleNavigate} />,
-			'login' : <LoginPage navigate={this.handleNavigate} />,
-			'loginEmail' : <LoginEmailPage navigate={this.handleNavigate} />,
-			'register' : <RegistrationPage navigate={this.handleNavigate} />,
-			'browse' : <BrowsePage navigate={this.handleNavigate} />,
-			'details' : <DetailsPage navigate={this.handleNavigate} />
-		};
+		// var auth = {
+		// 	signIn : this.signIn,
+		// 	signOut : this.signOut
+		// }
 
-		window.addEventListener('hashchange', function () {
-			var key = window.location.hash.slice(1);
+		// var pages = {
+		// 	'home' : <HomePage navigate={this.handleNavigate} auth={auth} />,
+		// 	'login' : <LoginPage navigate={this.handleNavigate} auth={auth} />,
+		// 	'loginEmail' : <LoginEmailPage navigate={this.handleNavigate} auth={auth} />,
+		// 	'register' : <RegistrationPage navigate={this.handleNavigate} auth={auth} />,
+		// 	'registerEmail' : <RegistrationEmailPage navigate={this.handleNavigate} auth={auth} />,
+		// 	'browse' : <BrowsePage navigate={this.handleNavigate} auth={auth} />,
+		// 	'details' : <DetailsPage navigate={this.handleNavigate} auth={auth} />
+		// };
 
-			if (!pages.hasOwnProperty(key)) key = 'home';
+		this.createPages();
 
-			this.setState({ currentPage : pages[key] });
-		}.bind(this), false);
+		window.addEventListener('hashchange', this.handleHashChange.bind(this), false);
 
 		this.simulateHashChange('#home');		
 	},
@@ -54,11 +56,65 @@ var DreamSpark = React.createClass({
 			document.body.style['background-color'] = '#ffffff';
 		}
 
+		var signOut = this.state.authenticated ? (
+			<li><a href="#" onClick={this.signOut}>Sign Out</a></li>
+		) : '';
+
 		return (
-			<div id='dreamspark-page-wrapper' className='container-fluid'>
-				{page}
+			<div>
+				<nav className='navbar navbar-inverse' role='navigation' style={{ 'margin-bottom' : '0', 'border-radius' : '0' }}>
+					<div className='container-fluid'>
+						<div className="navbar-header">
+					        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						        <span className="sr-only">Toggle navigation</span>
+						        <span className="icon-bar"></span>
+						        <span className="icon-bar"></span>
+						        <span className="icon-bar"></span>
+					        </button>
+					      <a className="navbar-brand" href="#">DreamSpark</a>
+					    </div>
+
+					    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					        <ul className="nav navbar-nav navbar-right">
+						        {signOut}
+					        </ul>
+					    </div>
+					</div>
+				</nav>
+
+				<div id='dreamspark-page-wrapper' className='container-fluid'>
+					{page}
+				</div>
 			</div>
 		);
+	},
+	createPages: function () {
+		var auth = {
+			signIn : this.signIn,
+			signOut : this.signOut
+		}
+
+		this.pages = this.pages ||  {
+			'home' : <HomePage navigate={this.handleNavigate} auth={auth} />,
+			'login' : <LoginPage navigate={this.handleNavigate} auth={auth} />,
+			'loginEmail' : <LoginEmailPage navigate={this.handleNavigate} auth={auth} />,
+			'register' : <RegistrationPage navigate={this.handleNavigate} auth={auth} />,
+			'registerEmail' : <RegistrationEmailPage navigate={this.handleNavigate} auth={auth} />,
+			'browse' : <BrowsePage navigate={this.handleNavigate} auth={auth} />,
+			'details' : <DetailsPage navigate={this.handleNavigate} auth={auth} />
+		};
+	},
+	handleHashChange: function () {
+		var key = window.location.hash.slice(1);
+
+		if (this.requiresAuth(key) && !this.state.authenticated) {
+			window.location.hash = '#login';
+			return;
+		}
+
+		if (!this.pages.hasOwnProperty(key)) key = 'home';
+
+		this.setState({ currentPage : this.pages[key] });
 	},
 	handleNavigate: function (newHash) {
 		window.location.hash = newHash;
@@ -71,6 +127,22 @@ var DreamSpark = React.createClass({
 			});
 
 		window.dispatchEvent(event);
+	},
+	signIn: function (redirect) {
+		this.setState({ authenticated : true });
+
+		if (redirect) {
+			window.location.hash = redirect;
+		}
+	},
+	signOut: function () {
+		alert ('You\'ve been signed out!');
+
+		this.setState({ authenticated : false });
+		window.location.hash = 'home';
+	},
+	requiresAuth: function (hash) {
+		return ['browse', 'details'].contains(hash);
 	}
 });
 
@@ -81,11 +153,7 @@ var HomePage = React.createClass({
 		};
 	},
 	render: function() {
-		var Nav = ReactBootstrap.Nav,
-			NavItem = ReactBootstrap.NavItem,
-			Jumbotron = ReactBootstrap.Jumbotron,
-			TabbedArea = ReactBootstrap.TabbedArea,
-			TabPane = ReactBootstrap.TabPane;
+		var Jumbotron = ReactBootstrap.Jumbotron;
 
 		function handleNavSelect(selectedKey) {
 			alert('Selected nav item: ' + selectedKey);
@@ -121,7 +189,7 @@ var HomePage = React.createClass({
 					<div className='col-md-10' style={{ padding : '0' }}>
 						<Jumbotron>
 							<div style={{ position : 'relative', height : '100%' }}>
-								<h1>{selectedItem.detailText}</h1>
+								<h1 style={{ 'margin-top' : '0' }}>{selectedItem.detailText}</h1>
 								<div id='button-wrapper' style={{ position : 'absolute', bottom : '10px', left : '10px' }}>
 									<button className='btn btn-lg' onClick={this.handleRegisterClick}>Register</button>
 									<button className='btn btn-lg' onClick={this.handleLoginClick} style={{ 'margin-left' : '1em' }} >Log In</button>
@@ -177,7 +245,7 @@ var LoginPage = React.createClass({
 		var nav = e.target.dataset.nav;
 
 		if (nav === 'oauth') {
-			this.props.navigate('browse');
+			this.props.auth.signIn('browse');
 		} else if (nav === 'email') {
 			this.props.navigate('loginEmail');
 		}
@@ -199,7 +267,7 @@ var LoginEmailPage = React.createClass({
 	},
 	handleLoginClick: function () {
 		if (this.refs.email.getDOMNode().value.trim() && this.refs.password.getDOMNode().value.trim()) {
-			this.props.navigate('browse');
+			this.props.auth.signIn('browse');
 		} else {
 			alert ('Email and password are required fields!');
 		}
@@ -210,9 +278,46 @@ var RegistrationPage = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<h1>RegistrationPage Page!</h1>
+				<form className='login-form' action={'javascript:void(0);'}>
+					<button onClick={this.handleItemClick} className='btn btn-lg btn-primary btn-block' style={{ 'margin-top' : '10px' }} data-nav='oauth'>Register With Microsoft</button>
+					<button onClick={this.handleItemClick} className='btn btn-lg btn-primary btn-block' style={{ 'margin-top' : '10px' }} data-nav='oauth'>Register With Google</button>
+					<button onClick={this.handleItemClick} className='btn btn-lg btn-primary btn-block' style={{ 'margin-top' : '10px' }} data-nav='oauth'>Register With Facebook</button>
+					<button onClick={this.handleItemClick} className='btn btn-lg btn-primary btn-block' style={{ 'margin-top' : '10px' }} data-nav='oauth'>Register With Twitter</button>
+					<button onClick={this.handleItemClick} className='btn btn-lg btn-primary btn-block' style={{ 'margin-top' : '10px' }} data-nav='email'>Register With Email</button>
+				</form>
 			</div>
 		);
+	},
+	handleItemClick: function (e) {
+		var nav = e.target.dataset.nav;
+
+		if (nav === 'oauth') {
+			this.props.auth.signIn('browse');
+		} else if (nav === 'email') {
+			this.props.navigate('registerEmail');
+		}
+	}
+});
+
+var RegistrationEmailPage = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<form className='login-form' action={'javascript:void(0);'}>
+					<h2 style={{ 'margin-bottom' : '10px' }}>Please sign in</h2>
+			        <input ref='email' type='email' className='form-control' placeholder='Email address' required='' autofocus='' />
+			        <input ref='password' type='password' className='form-control' placeholder='Password' required='' />
+			        <button onClick={this.handleLoginClick} className='btn btn-lg btn-primary btn-block' style={{ 'margin-top' : '10px' }}>Register</button>
+				</form>
+			</div>
+		);
+	},
+	handleLoginClick: function () {
+		if (this.refs.email.getDOMNode().value.trim() && this.refs.password.getDOMNode().value.trim()) {
+			this.props.auth.signIn('browse');
+		} else {
+			alert ('Email and password are required fields!');
+		}
 	}
 });
 
@@ -228,9 +333,47 @@ var BrowsePage = React.createClass({
 
 var DetailsPage = React.createClass({
 	render: function() {
+		var productTitle = this.props.productTitle || 'Software Product Title';
+
+		var Jumbotron = ReactBootstrap.Jumbotron;
+
 		return (
 			<div>
-				<h1>Details page!</h1>
+				<div className='row'>
+					<div class='col-md-12'>
+						<Jumbotron>
+							<div className='col-md-4'>
+								<img style={{ 'width' : '80%', 'height' : '80%', 'margin' : '20px', 'border' : '1px solid #000000' }} src='http://placehold.it/350x350' />
+							</div>
+							<div className='col-md-8' style={{ 'height' : '100%' }}>
+								<div style={{ 'position' : 'relative', 'height' : '100%' }}>
+									<h1>{productTitle}</h1>
+									<div id='button-wrapper' style={{ position : 'absolute', bottom : '10px', left : '10px' }}>
+										<button className='btn btn-lg'>Download Now</button>
+									</div>
+								</div>
+							</div>
+						</Jumbotron>
+					</div>
+				</div>
+				<div className='row' style={{ 'margin-bottom' : '20px' }}>
+					<div className='col-md-6' style={{ 'border-right' : '1px solid #000000' }}>
+						<h3>Notes</h3>
+						<p style={{ 'text-align' : 'justify' }}>{LIPSUM}</p>
+					</div>
+					<div className='col-md-6'>
+						<h3>Other Downloads</h3>
+						<p style={{ 'text-align' : 'justify' }}>
+							<ul style={{ 'list-style' : 'none', 'padding-left' : '0' }}>
+								<li>Windows (x86)</li>
+								<li>Windows (x64)</li>
+								<li>Mac OS X</li>
+								<li>Linux (x86)</li>
+								<li>Linux (x64)</li>
+							</ul>
+						</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
