821896b5e3e30bda8f96273fb6193689e00b6104
Matt Walston <m@mlw.ac>
2014-11-06 23:23:15 -0500

    Removed unused hamburger icon on mobile small screens

--- DreamSpark.jsx
+++ DreamSpark.jsx
@@ -65,12 +65,6 @@ var DreamSpark = React.createClass({
 				<nav className='navbar navbar-inverse' role='navigation' style={{ 'margin-bottom' : '0', 'border-radius' : '0' }}>
 					<div className='container-fluid'>
 						<div className="navbar-header">
-					        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-						        <span className="sr-only">Toggle navigation</span>
-						        <span className="icon-bar"></span>
-						        <span className="icon-bar"></span>
-						        <span className="icon-bar"></span>
-					        </button>
 					      <a className="navbar-brand" href="#">DreamSpark</a>
 					    </div>
 
