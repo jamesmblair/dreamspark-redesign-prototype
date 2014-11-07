ce5db20f8843a34f6d00c9b247e01e5a1a2f0783
Matt Walston <m@mlw.ac>
2014-11-06 22:38:46 -0500

    Added slick slider thing

--- DreamSpark.jsx
+++ DreamSpark.jsx
@@ -322,13 +322,47 @@ var RegistrationEmailPage = React.createClass({
 });
 
 var BrowsePage = React.createClass({
+	componentDidMount: function() {
+			$('.responsive').slick({
+				dots: true,
+				infinite: false,
+				speed: 300,
+				slidesToShow: 4,
+				slidesToScroll: 4,
+				responsive: [
+					{
+						breakpoint: 1024,
+						settings: {
+							slidesToShow: 3,
+							slidesToScroll: 3,
+							infinite: true,
+							dots: true
+						}
+					},
+					{
+						breakpoint: 600,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 2
+						}
+					},
+					{
+						breakpoint: 480,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1
+						}
+					}
+				]
+			});
+	},
 	render: function() {
 		return (
 			<div style={{ 'text-align' : 'center' }}>
 				<div className='row'>
 					<div className='col-md-12'>
 						<h2>Dev Tools</h2>
-							<div class="slider responsive">
+							<div className="slidingThing slider responsive">
 								<div><h3>1</h3></div>
 								<div><h3>2</h3></div>
 								<div><h3>3</h3></div>
